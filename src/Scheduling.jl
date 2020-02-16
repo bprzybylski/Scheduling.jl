ea6f4b5e355e19e442b40795ec084f65b6f32303
Bartek Przybylski <bprzybylski@users.noreply.github.com>
2020-02-16 14:33:57 +0100

    JobAssignment struct

--- src/Scheduling.jl
+++ src/Scheduling.jl
@@ -2,5 +2,6 @@ module Scheduling
 
 include("types/Job.jl")
 include("types/Jobs.jl")
+include("types/JobAssignment.jl")
 
 end # module
---
+++ src/types/JobAssignment.jl
@@ -0,0 +1,28 @@
+mutable struct JobAssignment
+    J::Job
+    M::UInt64     # machine number
+    S::Rational{UInt64} # starting time
+    C::Rational{UInt64} # completion time
+    function JobAssignment(J, M, S, C)
+        if S >= C
+            error("The execution time must be positive.")
+        end
+        return new(J, M, S, C)
+    end
+end
+
+function Base.show(io::IO, A::JobAssignment)
+    function rtos(r::Rational)
+        if r == Inf
+            return "∞"
+        elseif denominator(r) == 1
+            return string(numerator(r))
+        end
+        return string(numerator(r), "//", denominator(r))
+    end
+
+    printstyled(io, "$(A.J.name)"; bold = true, color = :light_blue)
+    print(io, " → ")
+    printstyled(io, "M$(A.M)"; bold = true, color = :light_yellow)
+    print(io, "[$(rtos(A.S)), $(rtos(A.C)))")
+end
