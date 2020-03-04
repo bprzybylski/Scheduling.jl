f9f541d553c0eb06c8e051f1e9adc302d692886b
Bartek Przybylski <bprzybylski@users.noreply.github.com>
2020-03-04 15:53:25 +0100

    Examples update

--- examples/P__Cmax.jl
+++
@@ -1,9 +0,0 @@
-using Random
-using Scheduling, Scheduling.Algorithms
-
-Random.seed!(12345)
-
-S = Algorithms.P__Cmax_IP(Jobs(rand(1:100, rand(1:20))), Machines(rand(2:5)))
-
-# Plot the schedule
-Scheduling.plot(S)
