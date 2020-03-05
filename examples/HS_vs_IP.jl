11315caa70297aaaf26f1ccea19a022bffe06788
Bartek Przybylski <bprzybylski@users.noreply.github.com>
2020-03-05 17:23:58 +0100

    Examples update

--- examples/HS_vs_IP.jl
+++ examples/HS_vs_IP.jl
@@ -15,18 +15,14 @@ for instance_id in 1:20
     # Generate an exact solution
     S1 = Algorithms.P__Cmax_IP(J, M)
     # Generate an approximation
-    S2 = Algorithms.P__Cmax_HS(J, M, eps = eps, verbose = true)
+    S2 = Algorithms.P__Cmax_HS(J, M, eps = eps)
 
+    # Calculate the approximation ratio
     approx_ratio = cmax(S2)//cmax(S1)
 
-    if approx_ratio < 1 || approx_ratio > 1 + eps
-        println("Instance $(instance_id):")
-        println("   Are we happy?:  NO")
-        println("   IP:             $(float(cmax(S1)))")
-        println("   HS:             $(float(cmax(S2)))")
-        println("   Approx. ratio:  $(float(approx_ratio))")
-        println(S2)
-    else
-        println("Instance $(instance_id) OK")
-    end
+    # Print the summary
+    println("Instance $(instance_id):")
+    println("   IP:             $(float(cmax(S1)))")
+    println("   HS:             $(float(cmax(S2)))")
+    println("   Approx. ratio:  $(float(approx_ratio))")
 end
