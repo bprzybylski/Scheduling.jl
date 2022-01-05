acd1dca3bdb5a7962e602a752098f0cef635a09d
Sascha Hunold <sascha@hunoldscience.net>
2022-01-05 08:43:59 +0100

    Newplots (#10)
    * bump plots/pyplot
    
    * Version bump
    
    * julia 1.5 required
    
    * Update Project.toml
    
    newline missing
    
    * fix algorithm names
    
    Co-authored-by: Bartek Przybylski <bprzybylski@users.noreply.github.com>

--- examples/P2__Cmax/SW3_tester.jl
+++ examples/P2__Cmax/SW3_tester.jl
@@ -7,7 +7,7 @@ Random.seed!(123)
 J = Jobs([27, 19, 19, 4, 48, 38, 29, 21, 9, 22, 11, 27, 36, 34, 21, 7, 7, 28])
 M = Machines(2)
 
-sched = Algorithms.SW3(J, M, eps=1//10)
+sched = Algorithms.P2__Cmax_SW3(J, M, eps=1//10)
 
 println("Cmax: $(Float64(cmax(sched)))")
 
